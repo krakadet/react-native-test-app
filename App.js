cbcfdc8fb7ad4b3e94afd4a61b4ee9eb204b7ac1
Krakadet <krakadet@gmail.com>
2020-04-20 13:56:45 +0300

    add react-navigator

--- App.js
+++ App.js
@@ -5,14 +5,14 @@
  * @format
  * @flow strict-local
  */
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import Main from './components/main';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 import store from './rootReduser';
 
@@ -21,13 +21,13 @@ export default class App extends Component {
     console.log('store=>', store.getState());
     return (
       <Provider store={store}>
-        {/*<NavigationContainer>*/}
-        {/*  <Stack.Navigator>*/}
-        {/*    <Stack.Screen name="Main" component={Main} />*/}
-        {/*    /!*<Stack.Screen name="Profile" component={Profile} />*!/*/}
-        {/*  </Stack.Navigator>*/}
-        {/*</NavigationContainer>*/}
-        <Main />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Main" component={Main} />
+            {/*<Stack.Screen name="Profile" component={Profile} />*/}
+          </Stack.Navigator>
+        </NavigationContainer>
+        {/*<Main />*/}
       </Provider>
     );
   }
---
+++ components/picture.js
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export const Picture = () => {
+  return (
+    <View>
+      <Text>photo</Text>
+    </View>
+  );
+};
